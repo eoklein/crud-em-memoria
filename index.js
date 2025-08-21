61ca373b7fd31e80720641e3cfd9b028dc6f3c83
eoklein <otimosprimevascodagama@gmail.com>
2025-08-20 22:24:39 -0300

    .

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+let ultimoId = 1;
 const usuario_admin = {
+    id: ultimoId,
     nome: "admin",
     email: "admin@admin",
 };
@@ -12,7 +14,25 @@ let usuarios = [usuario_admin];
 
 app.get('/usuarios', (req, res) => {
     res.json(usuarios).status(200);
-})
+});
+
+app.post('/usuarios', (req, res) => {
+    //pegar nome e email
+    //do body
+    const { nome, email } = req.body;
+    console.log(nome);
+    console.log(email);
+    if (!nome || !email) {
+        return res.status(400).json({ erro: "Nome e email são obrigatórios" });
+    }
+
+    //definir o id dele
+    //adicionar ele na lista/banco de dados
+
+    //atualizar o ultimoId
+    //retornar para o front se deu sucesso(201)
+
+});
 
 app.listen(3000);
 
