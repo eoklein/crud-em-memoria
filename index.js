17ce4935986b1342f68419fead7ddd5a74fa80a8
eoklein <otimosprimevascodagama@gmail.com>
2025-08-27 22:24:55 -0300

    .

--- index.js
+++ index.js
@@ -35,23 +35,78 @@ app.post('/usuarios', (req, res) => {
     usuarios.push(novoUsuario);
     ultimoId += 1;
 
-    return res.status(201).json(novoUsuario);
-
-    //definir o id dele
-    //adicionar ele na lista/banco de dados
-
-    //atualizar o ultimoId
-    //retornar para o front se deu sucesso(201)
+    return res.status(201).json(novoUsuario.id);
 
 });
+
+app.delete('/usuarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const idNumerico = parseInt(id);
+    
+    if (isNaN(idNumerico)) {
+        return res
+            .status(400)
+            .json({ mensagem: "ID inválido" });
+    }
+
+    let posicao_do_usuario = usuarios.findIndex((usuario) => usuario.id === idNumerico);
+    if (posicao_do_usuario === -1) {
+        return res.status(404).json({ mensagem: "Usuário não encontrado" });
+    }
+
+    usuarios.splice(posicao_do_usuario, 1);
+    return res.status(204).send();
+});
+
+app.patch('/usuarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        return res.status(400).json({ mensagem: "ID inválido" });
+    }
+
+    const usuario = usuarios.find((usuario) => usuario.id === id);
+
+    if (!usuario) {
+        return res.status(404).json({ mensagem: "Usuário não encontrado" });
+    }
+
+    const { nome, email } = req.body;
+
+    if (!nome && !email) {
+        return res.status(400).json({ mensagem: "Nome ou email são obrigatórios" });
+    }
+
+    if (email) {
+        let email_existe = usuarios.findIndex((usuario) => usuario.email === email);
+
+        if (email_existe !== -1) {
+            return res.status(400).json({ mensagem: "Email já cadastrado" });
+        }
+
+        usuario.email = email;
+
+        return res.status(200).json(usuario);
+    }
+
+    if (nome) {
+        let usuario_existe = usuarios.findIndex((usuario) => usuario.nome === nome);
+
+        if (usuario_existe !== -1) {
+            return res.status(400).json({ mensagem: "Nome já cadastrado" });
+        }
+
+        usuario.nome = nome;
+
+        return res.status(200).json(usuario);
+    }
+});
+
+app.get ("/usuarios/:id", (req, res) => {
+    return usuarios.find((usuario) => usuario.id === parseInt(req.params.id));
+});    
+
 
 app.listen(3000);
 
 
-/**
- * CRUD em memória
- * criar uma rota para pegar todos os usuarios
- * criar uma rota para cadastrar um usuario
- * criar uma rota para deletar um usuario
- * criar uma rota para atualizar um usuario
- */
