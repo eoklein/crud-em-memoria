8c591d898f255fcd7a62d1e545c25cbff59ab13a
eoklein <otimosprimevascodagama@gmail.com>
2025-09-17 19:20:59 -0300

    .

--- controller/usuarios-controller.js
+++ controller/usuarios-controller.js
@@ -36,68 +36,34 @@ function criarUsuario(req, res) {
     res.status(201).json(novoUsuario.id);
 }
 
-function deletarUsuario(req, res) {
-    const id = req.params.id;
-    const idNumerico = parseInt(id);
-
-    if (isNaN(idNumerico)) {
-    return res
-        .status(400)
-        .json({ mensagem: "ID inválido, precisa ser um numero" });
+async function deletarUsuario(req, res) {
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+        return res.status(400).json({ mensagem: "ID inválido, precisa ser um numero" });
     }
-
-    let posicao_do_usuario = usuarios.findIndex(
-    (usuario) => usuario.id === idNumerico
-    );
-
-    if (posicao_do_usuario === -1) {
-    return res.status(404).json({ mensagem: "Usuario nao encontrado" });
-    }
-
-    usuarios.splice(posicao_do_usuario, 1);
+    await prisma.users.delete({where: {id: id}});
     res.status(204).send();
 }
 
-function atualizarUsuario(req, res) {
+async function atualizarUsuario(req, res) {
     const id = parseInt(req.params.id);
 
     if (isNaN(id)) {
-    return res
-        .status(400)
-        .json({ mensagem: "ID inválido, precisa ser um numero" });
+    return res.status(400).json({ mensagem: "ID inválido, precisa ser um numero" });
     }
 
-    const usuario = usuarios.find((usuario) => usuario.id === id);
-    if (!usuario) {
-    return res.status(404).json({ mensagem: "Usuario nao encontrado" });
-    }
+    const {nome, email, idade} = req.body;
 
-    const { nome, email } = req.body;
+    await prisma.users.update({
+        where: {id: id},
+        data: {
+            nome: nome,
+            email: email,
+            idade: idade
+        }
+    });
 
-    if (!nome && !email) {
-    return res.status(400).json({ mensagem: "manda pelo menos um dos dados" });
-    }
-
-    console.log(`antes de atualizar ${usuario}`);
-
-    if (email) {
-    let email_existe = usuarios.findIndex((usuario) => usuario.email === email);
-
-    if (email_existe !== -1) {
-        return res.status(409).json({ mensagem: "Email ja cadastrado" });
-    }
-
-    usuario.email = email;
-    console.log(`antes de atualizar EMAIL ${usuario}`);
-    }
-
-  
-    if (nome) {
-    usuario.nome = nome;
-    console.log(`antes de atualizar NOME ${usuario}`);
-    }
-
-    res.status(200).json(usuario);
+    res.status(204).send();
 }
 
 async function listarUsuarioId(req, res) {
